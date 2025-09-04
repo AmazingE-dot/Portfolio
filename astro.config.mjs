3583b5878617274bfb41754f691cbf85468a54f1
AmazingE-dot <carlos-eduardo-leon2@hotmail.com>
2025-09-03 23:30:29 -0500

    Add GitHub Actions for Astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    site: 'https://github.com/AmazingE-dot',
+    base: '/Portfolio',
   }
 });
