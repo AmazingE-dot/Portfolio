e69892f33ee3f29313eca827af195a0a44c815ec
AmazingE-dot <carlos-eduardo-leon2@hotmail.com>
2025-09-03 23:46:33 -0500

    Correción en donde se llaman el site y base porque los habia dejado dentro de vite:{} 🤭

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
   site: 'https://amazingE-dot.github.io/Portfolio/',
   base: '/Portfolio/',
-  }
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
